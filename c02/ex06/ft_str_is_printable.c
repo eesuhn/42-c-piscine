6845c9644fae8188465d7d6f96d212addc7cf518
Yi Hong Lim <yilim@u82z03s02.42kl.edu.my>
2024-01-16 18:43:57 +0800

    Push: 'c02'

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -6,8 +6,19 @@
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:18:42 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/16 17:22:24 by yilim            ###   ########.fr       */
+/*   Updated: 2024/01/16 18:24:00 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+int	ft_str_is_printable(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < 32 || *str > 126)
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 15:54:11 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/16 18:42:24 by yilim            ###   ########.fr       */
+/*   Created: 2024/01/16 18:32:44 by yilim             #+#    #+#             */
+/*   Updated: 2024/01/16 18:32:48 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
-{
-	char	*temp;
 
-	temp = dest;
-	while (n > 0 && *src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
-	}
-	while (n-- > 0)
-	{
-		*dest = '\0';
-		dest++;
-	}
-	return (temp);
-}
