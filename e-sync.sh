d3a8fbb575cb09538b0d1e5cb9d48eb59db39fd3
Yi Hong Lim <yilim@u80z04s03.42kl.edu.my>
2024-01-19 11:20:48 +0800

    Updated esync

--- e-sync.sh
+++ e-sync.sh
@@ -13,9 +13,14 @@ if [ -z "$2" ]; then
 	printf "${RED}Error:${NC} Source is not provided.\n\n"
 	exit 1
 fi
+if [ "$1" == "push" && -z "&3" ]; then
+	printf "${RED}Error:${NC} Commit message is not provided.\n\n"
+	exit 1
+fi
 
 action="$1"
 source="$2"
+commit_msg="$3"
 
 divider() {
 	echo "${PURPLE}----------${NC}"
@@ -37,7 +42,7 @@ push() {
 	fi
 	divider
 	git add .
-	git commit -m "Push: '$source'"
+	git commit -m "Push: '$source'\n$commit_msg"
 	divider
 	git push
 	divider
