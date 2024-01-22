3e56dccbba3fadb0ecd725e3e8b40d07fbebc91b
Yi Hong Lim <yilim@u82z03s02.42kl.edu.my>
2024-01-22 17:24:37 +0800

    Push: 'c02': Fixed ex10

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,38 +6,34 @@
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 11:05:09 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/18 18:47:14 by yilim            ###   ########.fr       */
+/*   Updated: 2024/01/22 17:24:12 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strlen(char *str)
 {
-	char	*ptr;
+	int	i;
 
-	ptr = str;
-	while (*ptr != '\0')
+	i = 0;
+	while (*(str + i))
 	{
-		ptr++;
+		i++;
 	}
-	return (ptr - str);
+	return (i);
 }
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int				size_src;
 	unsigned int	i;
 
-	size_src = ft_strlen(src);
 	i = 0;
-	if (!(*src))
-		return (size_src);
-	while (i < (size - 1))
+	if (size == 0)
+		return (0);
+	while (*(src + i) && i + 1 < size)
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		*(dest + i) = *(src + i);
 		i++;
 	}
-	*dest = '\0';
-	return (size_src);
+	*(dest + i) = '\0';
+	return (ft_strlen(src));
 }
