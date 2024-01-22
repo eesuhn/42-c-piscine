7d3b9fe5c964be4646d7c24410a2b059450c2018
Yi Hong Lim <yilim@u82z03s02.42kl.edu.my>
2024-01-22 13:25:31 +0800

    Push: 'c02': Updated ex11

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 12:35:01 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/18 15:17:17 by yilim            ###   ########.fr       */
+/*   Updated: 2024/01/22 13:24:22 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,24 @@ void	ft_putchar(char c)
 void	ft_putstr_non_printable(char *str)
 {
 	char	hex[2];
+	char	*ptr;
 
-	while (*str)
+	ptr = str;
+	while (*ptr)
 	{
-		if (*str >= ' ' && *str <= '~')
-			ft_putchar(*str);
+		if (*ptr >= ' ' && *ptr <= '~')
+			ft_putchar(*ptr);
 		else
 		{
-			hex[0] = (*str >> 4);
+			
+			hex[0] = (*ptr >> 4) & 0x0F;
 			hex[0] += (hex[0] < 10) * '0' + (hex[0] >= 10) * ('a' - 10);
-			hex[1] = (*str & 0x0F);
+			hex[1] = (*ptr & 0x0F);
 			hex[1] += (hex[1] < 10) * '0' + (hex[1] >= 10) * ('a' - 10);
 			ft_putchar('\\');
 			ft_putchar(hex[0]);
 			ft_putchar(hex[1]);
 		}
-		str++;
+		ptr++;
 	}
 }
--- c02/ex11/main.c
+++ c02/ex11/main.c
@@ -6,9 +6,11 @@
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 12:55:33 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/18 15:15:34 by yilim            ###   ########.fr       */
+/*   Updated: 2024/01/22 13:23:43 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	ft_putstr_non_printable(char *str);
 
@@ -16,7 +18,8 @@ int	main(void)
 {
 	char	*str;
 
-	str = "Hello\nWorld";
+	str = "Hello\n\t\x1B\xA2\xA2World";
+	printf("%s\n", str);
 	ft_putstr_non_printable(str);
 	return (0);
 }
