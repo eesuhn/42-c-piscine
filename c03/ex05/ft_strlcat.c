41d287e5f11e498940e127a073a816a3206de11e
Yi Hong Lim <yilim@u82z03s01.42kl.edu.my>
2024-01-23 19:22:38 +0800

    Push: 'c03': Fixed segfault

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:18:12 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/23 17:18:39 by yilim            ###   ########.fr       */
+/*   Updated: 2024/01/23 19:09:51 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,20 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	i = 0;
 	dest_len = 0;
 	tmp = dest;
-	while (*dest)
+	while (*dest && dest_len < size)
 	{
 		dest++;
 		dest_len++;
 	}
-	if (size < (dest_len + 1))
+	if (size <= dest_len)
 		return (ft_strlen(src) + size);
-	while (i < (size - dest_len - 1))
+	while (i < (size - dest_len - 1) && src[i])
 	{
 		*dest = src[i];
 		dest++;
 		i++;
 	}
-	if (*tmp)
-		if (dest_len <= size && size != 0 && *(dest - 1) != '\0')
-			*dest = '\0';
+	if (dest_len <= size && size != 0)
+		*dest = '\0';
 	return (dest_len + ft_strlen(src));
 }
