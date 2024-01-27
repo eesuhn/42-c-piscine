3e81d0f11ddffc50376ac35f0a019bbf08a933a2
Yi Hong Lim <yilim@u82z03s01.42kl.edu.my>
2024-01-27 11:31:53 +0800

    Push: 'c07': Cleaned up ex03

---
+++ c07/ex03/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 12:56:10 by yilim             #+#    #+#             */
+/*   Updated: 2024/01/27 11:31:12 by yilim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strjoin(int size, char **strs, char *sep);
+
+int	main(void)
+{
+	int		size;
+	int		**strs = {"Hello", "World"};
+	char	*sep = " ";
+
+	return (0);
+}
