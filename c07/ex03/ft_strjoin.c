e2609880f7a005e23a31d4e2ea79f49275f28f21
Yi Hong Lim <yilim@u82z03s01.42kl.edu.my>
2024-01-27 19:21:29 +0800

    Push: 'c07': Done ft_strjoin

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,12 +6,75 @@
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 12:54:13 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/27 11:06:24 by yilim            ###   ########.fr       */
+/*   Updated: 2024/01/27 19:20:21 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	return (i);
+}
+
+void	ft_strcat(char *dest, char *src)
+{
+	int	d;
+	int	s;
+
+	d = 0;
+	s = -1;
+	while (*(dest + d))
+		d++;
+	while (*(src + ++s))
+	{
+		*(dest + d) = *(src + s);
+		d++;
+	}
+	*(dest + d) = '\0';
+}
+
+int	ft_totallen(int size, char **strs, char *sep)
+{
+	int	len;
+	int	i;
+
+	len = 0;
+	i = -1;
+	while (++i < size)
+	{
+		len += ft_strlen(strs[i]);
+		if (i < size - 1 && *sep)
+			len += ft_strlen(sep);
+	}
+	return (len);
+}
+
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
+	int		len;
+	char	*join;
+	int		i;
+
+	if (!size || !strs)
+	{
+		join = (char *) malloc(1);
+		join[0] = '\0';
+		return (join);
+	}
+	len = ft_totallen(size, strs, sep);
+	join = (char *) malloc(len + 1);
+	i = 0;
+	ft_strcat(join, strs[i]);
+	while (++i < size)
+	{
+		ft_strcat(join, sep);
+		ft_strcat(join, strs[i]);
+	}
+	return (join);
 }
--- c07/ex03/main.c
+++ c07/ex03/main.c
@@ -6,20 +6,27 @@
 /*   By: yilim <yilim@student.42KL.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 12:56:10 by yilim             #+#    #+#             */
-/*   Updated: 2024/01/27 11:31:12 by yilim            ###   ########.fr       */
+/*   Updated: 2024/01/27 19:20:33 by yilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+/*
 #include <stdlib.h>
+#include <unistd.h>
 
 char	*ft_strjoin(int size, char **strs, char *sep);
 
 int	main(void)
 {
-	int		size;
-	int		**strs = {"Hello", "World"};
-	char	*sep = " ";
+	int		size = 3;
+	char	*strs[] = {"Hello", "world", "!"};
+	char	*sep = ", ";
+	char	*join = ft_strjoin(size, strs, sep);
+	int		j = -1;
 
+	while (*(join + ++j))
+		write(1, (join + j), 1);
+	free(join);
 	return (0);
 }
+*/
